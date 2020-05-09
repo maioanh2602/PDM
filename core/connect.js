a2d4e69542b4182b7d40b2aa0bd10ddb0600f564
Jerry Nguyen <nguyentruongdat1661999@gmail.com>
2020-05-10 03:46:41 +0700

    3:46

--- core/connect.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-
-// Add the credentials to access your database
-var connection = mysql.createConnection({ 
-    host     : 'hcmiuiot.tech',
-    user     : 'root',
-    password :  'rootpw', 
-    database : 'library_schema'
-}); //Unchangable
---
+++ src/core/main.js
@@ -0,0 +1,67 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const url = require("url");
+const remote = require("electron").remote;
+let windows;
+
+
+//this function will create window
+function createWindow() {
+    // create BrowserWindow
+    windows = new BrowserWindow({
+        width: 1366,
+        height: 768,
+        // closable: false,
+        icon: "../img/people.ico",
+        frame: false,
+        show: false,
+        resizable: false,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    });
+    windows.setMenu(null);
+    // load index.html
+    windows.loadURL(
+        url.format({
+            pathname: path.join(__dirname, "../index.html"),
+            protocol: "file:",
+            slashes: true,
+        })
+    );
+
+    windows.once("ready-to-show", () => {
+        windows.show();
+    });
+    // Show devtools
+    windows.webContents.openDevTools();
+
+    windows.on("close", () => {
+        win = null;
+    });
+
+
+}
+//allow renderer process reuse
+app.allowRendererProcessReuse = true;
+
+// run create windows function
+app.on("ready", createWindow);
+
+app.on("close", function (event) {
+    event.preventDefault();
+});
+
+// quit when all windows are close
+app.on("window-all-closed", () => {
+    if (process.platform != "darwin") {
+        app.quit();
+    }
+});
+
+//activate
+app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
